8f3e3afa5f6df3e3a2699c912bed9df797015f80
TRalexxx <110291348+TRalexxx@users.noreply.github.com>
2023-05-13 10:56:41 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace classwork_13._05_localization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.loginL = new System.Windows.Forms.Label();
+            this.passL = new System.Windows.Forms.Label();
+            this.passConfirmL = new System.Windows.Forms.Label();
+            this.bdateL = new System.Windows.Forms.Label();
+            this.emailL = new System.Windows.Forms.Label();
+            this.loginTB = new System.Windows.Forms.TextBox();
+            this.passTB = new System.Windows.Forms.TextBox();
+            this.passConfirmTB = new System.Windows.Forms.TextBox();
+            this.bdatePicker = new System.Windows.Forms.DateTimePicker();
+            this.emailTB = new System.Windows.Forms.TextBox();
+            this.enBtn = new System.Windows.Forms.Button();
+            this.gerBtn = new System.Windows.Forms.Button();
+            this.ruBtn = new System.Windows.Forms.Button();
+            this.ukrBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginL
+            // 
+            resources.ApplyResources(this.loginL, "loginL");
+            this.loginL.Name = "loginL";
+            // 
+            // passL
+            // 
+            resources.ApplyResources(this.passL, "passL");
+            this.passL.Name = "passL";
+            // 
+            // passConfirmL
+            // 
+            resources.ApplyResources(this.passConfirmL, "passConfirmL");
+            this.passConfirmL.Name = "passConfirmL";
+            // 
+            // bdateL
+            // 
+            resources.ApplyResources(this.bdateL, "bdateL");
+            this.bdateL.Name = "bdateL";
+            // 
+            // emailL
+            // 
+            resources.ApplyResources(this.emailL, "emailL");
+            this.emailL.Name = "emailL";
+            // 
+            // loginTB
+            // 
+            resources.ApplyResources(this.loginTB, "loginTB");
+            this.loginTB.Name = "loginTB";
+            // 
+            // passTB
+            // 
+            resources.ApplyResources(this.passTB, "passTB");
+            this.passTB.Name = "passTB";
+            // 
+            // passConfirmTB
+            // 
+            resources.ApplyResources(this.passConfirmTB, "passConfirmTB");
+            this.passConfirmTB.Name = "passConfirmTB";
+            // 
+            // bdatePicker
+            // 
+            resources.ApplyResources(this.bdatePicker, "bdatePicker");
+            this.bdatePicker.Name = "bdatePicker";
+            // 
+            // emailTB
+            // 
+            resources.ApplyResources(this.emailTB, "emailTB");
+            this.emailTB.Name = "emailTB";
+            // 
+            // enBtn
+            // 
+            resources.ApplyResources(this.enBtn, "enBtn");
+            this.enBtn.Name = "enBtn";
+            this.enBtn.UseVisualStyleBackColor = true;
+            this.enBtn.Click += new System.EventHandler(this.enBtn_Click);
+            // 
+            // gerBtn
+            // 
+            resources.ApplyResources(this.gerBtn, "gerBtn");
+            this.gerBtn.Name = "gerBtn";
+            this.gerBtn.UseVisualStyleBackColor = true;
+            this.gerBtn.Click += new System.EventHandler(this.gerBtn_Click);
+            // 
+            // ruBtn
+            // 
+            resources.ApplyResources(this.ruBtn, "ruBtn");
+            this.ruBtn.Name = "ruBtn";
+            this.ruBtn.UseVisualStyleBackColor = true;
+            this.ruBtn.Click += new System.EventHandler(this.ruBtn_Click);
+            // 
+            // ukrBtn
+            // 
+            resources.ApplyResources(this.ukrBtn, "ukrBtn");
+            this.ukrBtn.Name = "ukrBtn";
+            this.ukrBtn.UseVisualStyleBackColor = true;
+            this.ukrBtn.Click += new System.EventHandler(this.ukrBtn_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ukrBtn);
+            this.Controls.Add(this.ruBtn);
+            this.Controls.Add(this.gerBtn);
+            this.Controls.Add(this.enBtn);
+            this.Controls.Add(this.emailTB);
+            this.Controls.Add(this.bdatePicker);
+            this.Controls.Add(this.passConfirmTB);
+            this.Controls.Add(this.passTB);
+            this.Controls.Add(this.loginTB);
+            this.Controls.Add(this.emailL);
+            this.Controls.Add(this.bdateL);
+            this.Controls.Add(this.passConfirmL);
+            this.Controls.Add(this.passL);
+            this.Controls.Add(this.loginL);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginL;
+        private System.Windows.Forms.Label passL;
+        private System.Windows.Forms.Label passConfirmL;
+        private System.Windows.Forms.Label bdateL;
+        private System.Windows.Forms.Label emailL;
+        private System.Windows.Forms.TextBox loginTB;
+        private System.Windows.Forms.TextBox passTB;
+        private System.Windows.Forms.TextBox passConfirmTB;
+        private System.Windows.Forms.DateTimePicker bdatePicker;
+        private System.Windows.Forms.TextBox emailTB;
+        private System.Windows.Forms.Button enBtn;
+        private System.Windows.Forms.Button gerBtn;
+        private System.Windows.Forms.Button ruBtn;
+        private System.Windows.Forms.Button ukrBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace classwork_13._05_localization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void enBtn_Click(object sender, EventArgs e)
+        {
+            ChangeLang("en");
+        }
+
+        private void ChangeLang(string lang)
+        {
+            ComponentResourceManager manager = new ComponentResourceManager(typeof(Form1));
+            CultureInfo culture = new CultureInfo(lang);
+            foreach (Control item in this.Controls)
+            {
+                manager.ApplyResources(item, item.Name, culture);
+            }
+        }
+
+        private void gerBtn_Click(object sender, EventArgs e)
+        {
+            ChangeLang("de");
+        }
+
+        private void ruBtn_Click(object sender, EventArgs e)
+        {
+            ChangeLang("ru");
+        }
+
+        private void ukrBtn_Click(object sender, EventArgs e)
+        {
+            ChangeLang("uk_UA");
+        }
+    }
+}
